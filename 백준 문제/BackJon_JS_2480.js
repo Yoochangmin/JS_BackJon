953ade165f30133b4d43d52dd49ddfba1981b895
Yoochangmin <cdals9141@gmail.com>
2023-09-13 01:02:09 +0900

    2480 complate

--- 백준 문제/BackJon_JS_2480.js
+++ 백준 문제/BackJon_JS_2480.js
@@ -1,24 +1,22 @@
 // 주사위 세개
+let fs = require('fs');
 
-let ran1 = Math.floor(Math.random()*6 )+1;
-let ran2 = Math.floor(Math.random()*6 )+1;
-let ran3 = Math.floor(Math.random()*6 )+1;
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let money=0;
-const arr=[];
+let ran1 = Number(input[0].split(" ")[0]);
+let ran2 = Number(input[0].split(" ")[1]);
+let ran3 = Number(input[0].split(" ")[2]);
 
-console.log(ran1, ran2, ran3)
-if (ran1 === ran2 || ran1 === ran3 ||ran2 === ran3){
-    money = 10000 + ran1 * 1000;
-}else if(ran1 === ran2 || ran2 ===ran3 || ran1 === ran3){
-    arr.push(ran1)
-    arr.push(ran2)
-    arr.push(ran3)
-    money = 1000 + n * 100;
-}else if( ran1 !== ran2 || ran2 !== ran3 || ran1 !== ran3){
-    maxMoney = Math.max(ran1,ran2,ran3)
-    money = maxMoney * 100;
+if (ran1 === ran2 && ran2 === ran3){
+    money = 10000 + ran2 * 1000;}
+else if(ran1 === ran2) {
+    money = 1000 + ran1 * 100;
+} else if(ran2 === ran3) {
+    money = 1000 + ran2 * 100;
+
+} else if(ran1 === ran3) {
+    money = 1000 + ran1 * 100;
+}else {
+    money= Math.max(ran1,ran2,ran3) *100;
 }
-console.log(arr);
-
 console.log(money)
